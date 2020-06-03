593a98a94f83725e34ba66f793da099f35553cc7
Niraj shrestha <CrestNiraj12>
2020-06-03 18:16:58 +0545

    Updated contact page

--- scripts/contact.js
+++ scripts/contact.js
@@ -8,19 +8,6 @@ $("nav2 > ul > li > a").on("click", function() {
     $("#nav2-2").removeClass("active-nav");
 });
 
-var texts = ["Feedback", "Offer", "Suggestion", "Question", "Support", "Recommendation"];
-var i = 0;
-
-function changeText() {
-    $(".container > h1 > span").text(texts[i]);
-    if (i == 5) i = 0;
-    if (++i < texts.length){
-        setTimeout(changeText, 2000);
-    }
-}
-
-changeText();
-
 $("form > button").on("click", (e) => {
     e.preventDefault();
     var error = false;
